d782b5d7434af404d64068fd2e9ad4ba520dbd78
tyrelcraig <craig.tyrel@gmail.com>
2017-07-20 11:07:08 -0700

    Added tyrelcraigpracticebot.py to the repo

---
+++ tyrelcraigpracticebot.py
@@ -0,0 +1,25 @@
+# Tyrel Craig Practice Bot.
+
+
+# Import the required packages.
+import praw
+
+# Choose which bot to run as named in the praw.ini file.
+
+reddit = praw.Reddit('TyrelCraigPracticeBot')
+
+# Choose which subreddit to run your bot in.
+
+subreddit = reddit.subreddit("learnpython")
+
+# Choose which submission category to  run your bot in.
+
+category = "hot"
+
+# Run through the top 5 submissions and report the following information.
+
+for submission in subreddit.category(limit=5):
+	print ("Title: ", submission.title)
+	print ("Text: ", submission.selftext)
+	print ("Score: ", submission.score)
+	print("-------------------- \n")
